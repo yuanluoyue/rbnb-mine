f733f9c4cecd38799148a73585b3adb67b11fddc
noname <yeunghuanyuan@qq.com>
2024-01-26 13:05:27 +0800

    refactor: move file to src

--- src/balance.js
+++ src/balance.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch')
 const csv = require('fast-csv')
 const fs = require('fs')
 
-const { sleepMS } = require('./lib')
-const { walletTablePath } = require('./config')
+const { sleepMS } = require('../lib')
+const { walletTablePath } = require('../config')
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
