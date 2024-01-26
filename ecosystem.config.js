c2dbdae7d3367170587b9e9fb2c6888c18df947f
noname <yeunghuanyuan@qq.com>
2024-01-26 13:55:07 +0800

    refactor: move file to src

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,17 @@ module.exports = {
   apps: [
     {
       name: '文件读写',
-      script: './listener.js',
+      script: './src/listener.js',
       instances: 1,
     },
     {
       name: '离线计算',
-      script: './calculater.js',
+      script: './src/calculater.js',
       instances: 2,
     },
     {
       name: '答案消费',
-      script: './sender.js',
+      script: './src/sender.js',
       instances: 1,
     },
   ],
